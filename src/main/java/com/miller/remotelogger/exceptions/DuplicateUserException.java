8da47cafb11985a9961e04a5760d080653dee641
Jonathan Miller <jonathan@winepos.com>
2015-04-21 17:56:03 -0400

    Exception Handling Classes
    Added Exception handling classes

---
+++ src/main/java/com/miller/remotelogger/exceptions/DuplicateUserException.java
@@ -0,0 +1,7 @@
+package com.miller.remotelogger.exceptions;
+
+public class DuplicateUserException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/miller/remotelogger/exceptions/LogEventNotFoundException.java
@@ -0,0 +1,7 @@
+package com.miller.remotelogger.exceptions;
+
+public class LogEventNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
